7430addaf17c7c18851ddaedef9e8474d8a9ad34
sun <516293816@qq.com>
2024-10-16 23:20:26 +0800

    add

---
+++ tongji.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?a3e14d6e51858ddd0d8e8c22d3d62add";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
