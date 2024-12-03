92e8e302cc9121c3620fce72a4008316cfe7a66a
sun <516293816@qq.com>
2024-12-03 21:27:32 +0800

    add

--- assets/tongji.js
+++ assets/tongji.js
@@ -1,9 +1,7 @@
-<script>
 var _hmt = _hmt || [];
-(function() {
+(function () {
   var hm = document.createElement("script");
   hm.src = "https://hm.baidu.com/hm.js?a3e14d6e51858ddd0d8e8c22d3d62add";
-  var s = document.getElementsByTagName("script")[0]; 
+  var s = document.getElementsByTagName("script")[0];
   s.parentNode.insertBefore(hm, s);
 })();
-</script>
